640a4e9591ca156a8b886b48b21ac53ecc785e44
linmin3 <linmin3@yy.com>
2020-04-20 11:37:08 +0800

    修复文件编码

--- src/core/ServiceTransPushIf.h
+++ src/core/ServiceTransPushIf.h
@@ -20,54 +20,54 @@ enum SmsTransCallErrType {
   CALL_ERR = 3,
 };
 
-//������
+//返回码
 enum SmsTransReturnCode {
   E_RETURN_INITLOG_FAIL = -7,
-  //��־��ʼ��ʧ��
+  //日志初始化失败
   E_RETURN_UID_ZERO = -6,
-  //UID ��0���޷�����
+  //UID 是0，无法启动
   E_RETURN_STREAM_NOT_READY = -5,
-  //����˫��������ʧ��
+  //传输双向流创建失败
   E_RETURN_CHANNEL_NOT_READY =- 4,
-  //����ͨ������ʧ��
+  //传输通道创建失败
   E_RETURN_TRANS_ERR_ARG = -3,
-  //����joinGroup/leaveGroup��ʱ�����������
+  //调用joinGroup/leaveGroup的时候参数有问题
   E_RETURN_STUB_NOT_READY = -2,
-  // closeservice��sendʱû�ж�Ӧ��type
+  // closeservice或send时没有对应的type
   E_RETURN_TRANS_EXIST = -1,
-  // openserviceʱ�Ѿ��ж�Ӧ��type��
+  // openservice时已经有对应的type了
   E_RETURN_SUCCESS = 0,
-  //�ӿڵ�������
+  //接口调用正常
   E_RETURN_NOT_INIT = 1,
-  //û�г�ʼ��,EduProto_Init���еķ�����
+  //没有初始化,EduProto_Init才有的返回码
   E_RETURN_NOT_REGISTER = 2,
-  //ûע��
+  //没注册
   E_RETURN_INIT_FAILED = 3,
-  //��ʼ��ʧ��
+  //初始化失败
   E_RETURN_NOPROTO = 4,
-  //û�м��ص�protoģ��
+  //没有加载到proto模块
   E_RETURN_ALREADYLOGINED = 5,
-  //�Ѿ���¼
+  //已经登录
   E_RETURN_NOTLOGINED = 6,
-  // UIDû�е�½�����ڲ��ǵ�¼״̬
-  // E_RETURN_RE_LOGIN = 7,//UID�Ѿ���½��
+  // UID没有登陆，现在不是登录状态
+  // E_RETURN_RE_LOGIN = 7,//UID已经登陆过
   E_RETURN_CALL_MUTEX = 8,
-  //���û���
+  //调用互斥
   E_RETURN_REQUEST_ERR = 9,
-  //������ʧ��
+  //请求发送失败
 };
 
 /**
-@brief ͨ�óɹ��ص�
-@param [in] data �Զ���ָ��
+@brief 通用成功回调
+@param [in] data 自定义指针
 */
 typedef void (*SmsTrans_SucCallback)(void* data);
 
 /**
-@brief ͨ��ʧ�ܻص�
-@param [in] code ������
-@param [in] desc ��������
-@param [in] data �Զ���ָ��
+@brief 通用失败回调
+@param [in] code 错误码
+@param [in] desc 错误描述
+@param [in] data 自定义指针
 */
 typedef void (*SmsTrans_ErrCallback)(SmsTransCallErrType code, const char* desc, void* data);
 
@@ -90,13 +90,13 @@ struct SmsTransUserInfo {
 
 typedef enum {
   E_LINK_CREATE_OK = 1,
-  //��·�����ɹ�
+  //链路创建成功
   E_LINK_CREATE_RETRY,
-  //��·����ʧ��������
+  //链路创建失败重试中
   E_LINK_LOGINTING,
   E_LINK_LOGIN_OK,
   E_LINK_LOGIN_FAIL,
-  // E_LINK_LOGIN_FAIL =4, ����Ӧ���ûص�������
+  // E_LINK_LOGIN_FAIL =4, 这种应该让回调来返回
   E_LINK_FAILURE,
   E_LINK_SHUTDOWN,
   E_LINK_LOGOUT_OK,
