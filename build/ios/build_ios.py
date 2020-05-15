484c09af02e128cf8fd37f9f7a4de51738177bac
linmin3 <linmin3@yy.com>
2020-05-15 14:41:22 +0800

    修复脚本问题

--- build/ios/build_ios.py
+++ build/ios/build_ios.py
@@ -91,7 +91,7 @@ for arch in arch_list:
         if not os.path.exists(protobuf_lib):
             protobuf_lib = BUILD_DIR + \
                 '/3rdparty/grpc/third_party/protobuf/cmake/libprotobuf.a'
-            protobuf_libs.append(protobuf_lib)
+        protobuf_libs.append(protobuf_lib)
 
         make_universal_list = []
 
