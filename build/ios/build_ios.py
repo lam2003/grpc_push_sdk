9fcb15ce26338656a6cbf46bafcc25947d28a4dc
linmin3 <linmin3@yy.com>
2020-06-18 16:33:08 +0800

    修改xcode版本

--- build/ios/build_ios.py
+++ build/ios/build_ios.py
@@ -18,7 +18,7 @@ arch_list = ['arm64',
              ]
 build_mode = ['Debug', 'Release']
 
-deployment_target = '8.0'
+deployment_target = '10.0'
 generator = 'Unix Makefiles'
 
 for mode in build_mode:
