991a7b5ff6ec94003888c6418a48d73f11fd4dd2
linmin3 <linmin3@yy.com>
2020-06-18 17:59:03 +0800

    ios构建先安装go(boringssl 测试用例依赖)

--- ci-build.py
+++ ci-build.py
@@ -6,19 +6,18 @@ import shutil
 import subprocess
 import multiprocessing
 
+
 def call(command, shell=False):
     print('calling:', str(command))
     res = subprocess.call(command, shell=shell)
     if res != 0:
         sys.exit(-1)
 
+
 platform = sys.argv[3]
 
 call("git submodule update --init --recursive", True)
 
 if platform.lower() == 'ios':
+    call("brew install go", True)
     call("cd build/ios/ && chmod +x ./build_ios.py && ./build_ios.py", True)
-
-
-
-
