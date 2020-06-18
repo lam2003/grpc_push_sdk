f5895156c10833d2dbaea1e70f4aab60fb48e3d9
linmin3 <linmin3@yy.com>
2020-06-18 17:04:26 +0800

    xcode 10.15

--- build/ios/build_ios.py
+++ build/ios/build_ios.py
@@ -18,7 +18,7 @@ arch_list = ['arm64',
              ]
 build_mode = ['Debug', 'Release']
 
-deployment_target = '10.4'
+deployment_target = '10.15'
 generator = 'Unix Makefiles'
 
 for mode in build_mode:
