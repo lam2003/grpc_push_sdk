df275c81f7b9a94d34c07521c8b557b2d02c0042
linmin3 <linmin3@yy.com>
2020-06-19 10:22:03 +0800

    update ci-build.py

--- ci-build.py
+++ ci-build.py
@@ -6,6 +6,7 @@ import shutil
 import subprocess
 import multiprocessing
 
+SPECS_NAME = 'EduPushSDK'
 
 def call_ignore_error(command, shell=False):
     print('call_ignore_error:', str(command))
@@ -28,19 +29,18 @@ platform = sys.argv[3]
 output_dir = sys.argv[7]
 
 
-# call_ignore_error("brew install go", True)
-# call_ignore_error("brew upgrade go", True)
-# call_ignore_error("brew install cmake", True)
-# call_ignore_error("brew upgrade cmake", True)
+call_ignore_error("brew install go", True)
+call_ignore_error("brew upgrade go", True)
+call_ignore_error("brew install cmake", True)
+call_ignore_error("brew upgrade cmake", True)
 
-# call("git submodule update --init --recursive", True)
-# if platform.lower() == 'ios':
-#     call(
-#         "cd build/ios/ && chmod +x ./build_ios.py && ./build_ios.py", True)
+call("git submodule update --init --recursive", True)
+if platform.lower() == 'ios':
+    call(
+        "cd build/ios/ && chmod +x ./build_ios.py && ./build_ios.py", True)
 
 makedirs(output_dir)
-makedirs("./out")
-print("################################# output_dir:", output_dir)
 
-copycmd = 'zip -r ' + str(output_dir) + '/out.zip ' + './out/*'
+copycmd = 'zip -r ' + str(output_dir) + '/' + \
+    str(SPECS_NAME)+'.zip ' + './out/*'
 call(copycmd, True)
